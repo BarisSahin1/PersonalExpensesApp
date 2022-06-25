a49845246d485a5c179866e4add28b803cf3fb86
BarisSahin <sahinbarisdev@gmail.com>
2022-06-25 13:11:59 +0300

    Function address and constructor are added

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
 
-class newTransaction extends StatelessWidget {
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(8),
+        padding: EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             TextField(
-              decoration: InputDecoration(labelText: "Title"),
-              /* onChanged: (value) {
-                      inputTitle = value;
-                    }, */
+              decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              // onChanged: (val) {
+              //   titleInput = val;
+              // },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
-              // onChanged: (value) => inputAmount = value,
               controller: amountController,
+              // onChanged: (val) => amountInput = val,
             ),
             FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
               onPressed: () {
-                print(titleController.text);
-                print(amountController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
               },
-              child: Text(
-                "Add Transaction",
-                style: TextStyle(
-                  color: Colors.purple,
-                ),
-              ),
             ),
           ],
         ),
